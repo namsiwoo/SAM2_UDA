e2ccb3c3603fac5336401b991ab797d13ca74cff
unknown <siwoonam@dgist.ac.kr>
2024-09-22 17:29:02 -0400

    first commit

--- train_sam2_gt.py
+++ train_sam2_gt.py
@@ -34,14 +34,13 @@ def main(args, device, class_list):
     import torch
 
     sam2_checkpoint = "sam2_hiera_small.pt"  # path to model weight
-    model_cfg = "sam2_configs/sam2_hiera_s_ssm.yaml"
-    model_cfg = "sam2_configs/sam2_hiera_s.yaml"
+    # model_cfg = "sam2_configs/sam2_hiera_s_ssm.yaml"
+    model_cfg = "sam2_hiera_s.yaml"
 
     sam2_model = build_sam2(model_cfg, sam2_checkpoint, device="cuda")  # load model
     predictor = SAM2ImagePredictor(sam2_model)
     predictor.model.sam_mask_decoder.train(True)
 
-    predictor = predictor.to(device)
 
     optimizer = torch.optim.AdamW(params=predictor.model.parameters(), lr=1e-5, weight_decay=4e-5)
     lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 20, eta_min=1.0e-7)
