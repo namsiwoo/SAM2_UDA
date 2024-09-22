b696a36b2503894f592929f6be4daf875348915d
unknown <siwoonam@dgist.ac.kr>
2024-09-22 17:15:44 -0400

    first commit

--- train_sam2_gt.py
+++ train_sam2_gt.py
@@ -34,7 +34,9 @@ def main(args, device, class_list):
     import torch
 
     sam2_checkpoint = "sam2_hiera_small.pt"  # path to model weight
-    with open("sam2_hiera_s.yaml", "r") as f:
+    model_cfg = yaml.load("sam2_hiera_s.yaml", Loader=yaml.FullLoader)
+
+    with open("sam2_configs/sam2_hiera_s.yaml", "r") as f:
         model_cfg = yaml.safe_load(f)
 
     sam2_model = SAM2Base(model_cfg['model'])
