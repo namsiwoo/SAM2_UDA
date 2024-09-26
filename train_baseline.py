6ae50f3792655ac47114340f61090d0c6657833a
unknown <siwoonam@dgist.ac.kr>
2024-09-25 21:23:51 -0400

    train_test

--- train_baseline.py
+++ train_baseline.py
@@ -333,6 +333,8 @@ def main(args, device, class_list):
                     max_miou = np.nanmean(ave_mIOUs)
 
                 print(epoch, ': ave mIOU\t{:s} (b mIOU: {}'.format(str(np.nanmean(ave_mIOUs)), max_miou))
+                if max_miou < 15:
+                    args.start_val = epoch + 10
 
 
 def test(args, device):
