184650124d0b19658282c7b8b7268411bb3eaec8
unknown <siwoonam@dgist.ac.kr>
2024-09-27 15:26:42 -0400

    train_test

--- sam2/modeling/sam2_ssm.py
+++ sam2/modeling/sam2_ssm.py
@@ -91,7 +91,7 @@ class SAM2Base(torch.nn.Module):
         # extra arguments used to construct the SAM mask decoder; if not None, it should be a dict of kwargs to be passed into `MaskDecoder` class.
         sam_mask_decoder_extra_args=None,
         compile_image_encoder: bool = False,
-        num_class: int = 14,
+        num_class: int = 12,
     ):
         super().__init__()
 
