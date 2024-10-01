029f730e418b6c7bc0cc8b33fb1872b937649276
unknown <siwoonam@dgist.ac.kr>
2024-10-01 00:43:15 -0400

    first commit

--- train_sam2_gt.py
+++ train_sam2_gt.py
@@ -186,9 +186,9 @@ def main(args, device, class_list):
             # predictor.set_image_batch(img)  # apply SAM image encoder to the image
 
             backbone_out = sam2_model.forward_image(img.to(device))
-            print(img.shape)
+
             _, vision_feats, _, _ = sam2_model._prepare_backbone_features(backbone_out)
-            vision_feats[-1] = vision_feats[-1] + sam2_model.no_mem_embed
+            print(vision_feats[::-1].shape)
             feats = [
                         feat.permute(1, 2, 0).view(img.shape[0], -1, *feat_size)
                         for feat, feat_size in zip(vision_feats[::-1], _bb_feat_sizes[::-1])
